5bca273ca28c5eaf40af7309a544f4d8ccee00a5
Iain Farrell <iain.farrell@gmail.com>
2013-08-04 17:38:25 +0100

    First drop

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<title>MotoGP Standings</title>
+
+		<link rel="stylesheet" type="text/css" href="../framework.css">
+  <style type="text/css">
+    body {
+      width: 384px;
+      padding: 0px;
+      margin: 0px;
+      font-family: sans-serif;
+    }
+    h1 {
+      word-wrap: break-word;
+      font-family:sans-serif;
+    }
+    table {
+      padding: 2px;
+      width:380px ;
+      border: 0px;
+    }
+  </style>
+	
+	</head>
+
+	<body>
+
+		<div id="wrap">
+
+			<header>
+
+				<!-- Put stuff here -->
+
+			</header>
+
+			<div id="main">
+
+<h1>MotoGP™ 2013</h1>
+<p>Top 5 riders after <b>9</b> races with <b>9</b> more to go.</p>
+
